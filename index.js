9e8715ec01ccfafa351f2e05ba6c44b82caf8a3f
Marcelo Santos <marcelosantos@Marcelos-MBP.fios-router.home>
2021-09-07 20:33:53 -0400

    managed to complete further tasks

--- index.js
+++ index.js
@@ -168,7 +168,33 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
+let computerInput = Math.random();
 
+function game(user, computer){
+  if (computerInput >= 0 && computerInput < 0.33) {
+    let computer = 'rock'
+  }else if (computerInput > 0.33 && computerInput < 0.66){
+    computer = 'paper';
+  }else if (computerInput > 0.66) {
+    computer = 'scissors';
+  }
+  if (user === computer){
+    return 'its a tie!';
+  }else if (user === 'paper' && computer === 'rock') {
+    return 'you win!';
+  }else if (user === 'rock' && computer === 'scissors') {
+    return 'you win!';
+  }else if (user === 'scissors' && computer === 'paper') {
+    return 'you win!';
+  }else if (user === 'rock' && computer === 'paper'){
+    return 'you lose!';
+  }else if (user === 'scissors' && computer === 'rock'){
+    return 'you lose!';
+  }else if (user === 'paper' && computer === 'scissors') {
+    return 'you lose!';
+  }
+}
+console.log(game(user,computer));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -245,7 +271,6 @@ function grade(score){
   }
 
 }
-console.log(grade(95));
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
